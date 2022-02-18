c3acebb9591a0dfbe453ad6099e16fd945b89230
Yoan Thirion <yoan.thirion@gmail.com>
2022-02-18 17:53:22 +0100

    Add chapter 2

--- c#/TestMoney.cs
+++ c#/TestMoney.cs
@@ -1,21 +1,45 @@
 using FluentAssertions;
 using Xunit;
 
-namespace money_problem;
-
-public class TestMoney
+namespace money_problem
 {
-    [Fact]
-    public void TestMultiplication()
+    public class TestMoney
     {
-        var fiver = new Dollar(5);
-        var tenner = fiver.Times(2);
+        [Fact(DisplayName = "5 USD x 2 = 10 USD")]
+        public void MultiplicationInUsd()
+        {
+            var fiveDollars = new Money(5, Currency.USD);
+            fiveDollars.Times(2)
+                .Should()
+                .Be(new Money(10, Currency.USD));
+        }
+    
+        [Fact(DisplayName = "10 EUR x 2 = 20 EUR")]
+        public void MultiplicationInEuro()
+        {
+            var tenEuros = new Money(10, Currency.EUR);
+            tenEuros.Times(2)
+                .Should()
+                .Be(new Money(20, Currency.EUR));
+        }
 
-        tenner.Amount.Should().Be(10);
+        [Fact(DisplayName = "4002 KRW / 4 = 1000.5 KRW")]
+        public void Division()
+        {
+            var originalMoney = new Money(4002, Currency.KRW);
+            originalMoney.Divide(4)
+                .Should()
+                .Be(new Money(1000.5, Currency.KRW));
+        }
     }
-}
 
-public record Dollar(int Amount)
-{
-    public Dollar Times(int multiplier) => new(Amount * multiplier);
+    public record Money(double Amount, Currency Currency)
+    {
+        public Money Times(int multiplier) => this with { Amount = Amount * multiplier };
+
+        public Money Divide(int divisor) => this with {Amount = Amount / divisor};
+        public override string ToString() => $"{Amount} {Currency}";
+    }
+
+    public enum Currency { EUR , USD, KRW }
 }
--- scala/src/test/scala/MoneyTest.scala
+++ scala/src/test/scala/MoneyTest.scala
@@ -1,12 +1,21 @@
 class MoneyTest extends org.scalatest.funsuite.AnyFunSuite {
-  test("TestMultiplication") {
-    val fiver = Dollar(5);
-    val tenner = fiver.Times(2);
-
-    assert(tenner.amount === 10)
+  test("5 USD x 2 = 10 USD") {
+    val fiveDollars = Money(5, "USD")
+    assert(fiveDollars.times(2) === Money(10, "USD"))
   }
 
-  case class Dollar(amount: Int) {
-    def Times(multiplier: Int): Dollar = Dollar(amount * multiplier)
+  test("10 EUR x 2 = 20 EUR") {
+    val tenEuros = Money(10, "EUR")
+    assert(tenEuros.times(2) === Money(20, "EUR"))
+  }
+
+  test("4002 KRW / 4 = 1000.5 KRW") {
+    val originalMoney = Money(4002, "KRW")
+    assert(originalMoney.divide(4) === Money(1000.5, "KRW"))
+  }
+
+  case class Money(amount: Double, currency: String) {
+    def times(multiplier: Int): Money = copy(amount = amount * multiplier)
+    def divide(divisor: Int) = copy(amount = amount / divisor)
   }
 }
