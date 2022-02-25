d195602ff0f7d096da3d45003b49a667ee02616a
Yoan Thirion <yoan.thirion@gmail.com>
2022-02-25 07:09:20 +0100

    Chapter 12 -> scala

--- scala/src/test/scala/BankShould.scala
+++ scala/src/test/scala/BankShould.scala
@@ -2,18 +2,27 @@ import domain.{Bank, Money}
 import org.scalatest.EitherValues
 
 class BankShould extends org.scalatest.funsuite.AnyFunSuite with EitherValues {
+  private val bank: Bank = Bank().addExchangeRate("EUR", "USD", 1.2)
+
   test("10 EUR -> USD = 12 USD") {
-    val bank = Bank().addExchangeRate("EUR", "USD", 1.2)
     assert(bank.convert(Money(10, "EUR"), "USD").value === Money(12, "USD"))
   }
 
   test("10 EUR -> EUR = 10 EUR") {
-    val bank = Bank()
     assert(bank.convert(Money(10, "EUR"), "EUR").value === Money(10, "EUR"))
   }
 
   test("Return a Left in case of missing exchange rates") {
-    val bank = Bank()
     assert(bank.convert(Money(10, "EUR"), "KRW").left.value === "EUR->KRW")
+  }
+
+  test("Conversion with different exchange rates EUR -> USD") {
+    assert(bank.convert(Money(10, "EUR"), "USD").value === Money(12, "USD"))
+    assert(
+      bank
+        .addExchangeRate("EUR", "USD", 1.3)
+        .convert(Money(10, "EUR"), "USD")
+        .value === Money(13, "USD")
+    )
   }
 }
