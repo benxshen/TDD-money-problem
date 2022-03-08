5bbb9bb6ba4d726179553ef59211f27a69757759
Yoan Thirion <yoan.thirion@gmail.com>
2022-03-08 16:33:04 +0100

    Chapter 4 -> java

--- java/src/test/java/money_problem/domain/MoneyTest.java
+++ java/src/test/java/money_problem/domain/MoneyTest.java
@@ -3,20 +3,11 @@ package money_problem.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static money_problem.domain.Currency.*;
+import static money_problem.domain.Currency.EUR;
+import static money_problem.domain.Currency.KRW;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MoneyTest {
-    @Test
-    @DisplayName("5 USD x 2 = 10 USD")
-    void shouldMultiplyInDollars() {
-        var fiveDollars = new Money(5, USD);
-        var tenDollars = fiveDollars.times(2);
-
-        assertThat(tenDollars)
-                .isEqualTo(new Money(10, USD));
-    }
-
     @Test
     @DisplayName("10 EUR x 2 = 20 EUR")
     void shouldMultiplyInEuros() {
