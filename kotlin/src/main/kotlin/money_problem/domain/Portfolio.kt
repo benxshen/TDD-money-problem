6e1cc08e1eef2c3ecfb183c3bdbd8e61e342c295
Yoan Thirion <yoan.thirion@gmail.com>
2022-03-08 07:40:43 +0100

    Chapter 9 -> kotlin

--- kotlin/src/main/kotlin/money_problem/domain/Portfolio.kt
+++ kotlin/src/main/kotlin/money_problem/domain/Portfolio.kt
@@ -1,6 +1,11 @@
 package money_problem.domain
 
-private const val euroToUSD = 1.2
+import money_problem.domain.Currency.*
+
+private val exchangeRates = mapOf(
+    keyFor(EUR, USD) to 1.2,
+    keyFor(USD, KRW) to 1100.0
+)
 
 data class Portfolio(val moneys: List<Money>)
 
@@ -8,4 +13,7 @@ fun Portfolio.evaluate(currency: Currency): Money =
     Money(moneys.fold(0.0) { acc, money -> acc + convert(money, currency) }, currency)
 
 private fun convert(money: Money, currency: Currency): Double =
-    if (currency == money.currency) money.amount else money.amount * euroToUSD
+    if (currency == money.currency) money.amount
+    else money.amount * exchangeRates[keyFor(money.currency, currency)]!!
+
+private fun keyFor(from: Currency, to: Currency): String = "$from->$to"
--- kotlin/src/test/kotlin/money_problem/domain/PortfolioTest.kt
+++ kotlin/src/test/kotlin/money_problem/domain/PortfolioTest.kt
@@ -1,5 +1,6 @@
 package money_problem.domain
 
+import money_problem.domain.Currency.KRW
 import money_problem.domain.Currency.USD
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -15,10 +16,17 @@ class PortfolioTest {
 
     @Test
     fun `5 USD + 10 EUR = 17 USD`() {
-
         val portfolio = Portfolio(listOf(5.0.dollars(), 10.0.euros()))
 
         assertThat(portfolio.evaluate(USD))
             .isEqualTo(17.0.dollars())
+    }
+
+    @Test
+    fun `1 USD + 1100 KRW = 2200 KRW`() {
+        val portfolio = Portfolio(listOf(1.0.dollars(), 1_100.0.koreanWons()))
+
+        assertThat(portfolio.evaluate(KRW))
+            .isEqualTo(2_200.0.koreanWons())
     }
 }
